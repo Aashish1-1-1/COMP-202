56eb2711a62b9ff361e96fd37e992e25adde6685
Aashish1-1-1 <aashishadhikrai693@gmail.com>
2024-04-09 23:24:16 -0500

    add header file

--- linkedlist.h
+++ linkedlist.h
@@ -1,6 +1,31 @@
-class Node{
-    int date;
-    Node *next;
+class Node
+{
+    public:
+        int data;
+        Node *next;
 
-    Node(){};
-}
+        Node(){}
+        Node(int d): data(d),next(nullptr) {}
+        Node(int d,Node*next): data(d),next(next) {}
+};
+
+class LinkedList
+{
+    private:
+        Node *HEAD;
+        Node *TAIL;
+    
+    public:
+        LinkedList();
+        ~LinkedList();
+
+        void add(Node *pred,int data);
+        void addToHead(int data);
+        void addToTail(int data);
+
+        bool remove(int data);
+        bool removeFromHead();
+        bool removeFromTail();
+        bool isEmpty();
+
+};
